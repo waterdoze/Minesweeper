0f87be260fc7a0145d877052c471ac1af451fa8e
waterdoze <71229170+waterdoze@users.noreply.github.com>
2020-09-20 00:43:57 -0500

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -17,166 +17,163 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePlay {
-	int[][] board;
-	int r, c;
-	JPanel mainpanel;
-	int count;
-	JLabel time;
-	JLabel bomb;
-	int bombCount;
-	Timer timer;
 	
+	int[][] board;
+	int r, c, bombCount, count;
+	JPanel mainpanel;
+	JLabel time, bomb;
+	Timer timer;
 	Al[][] test;
+
 	public GamePlay(int mines, int rows, int columns) {
 		r = rows;
 		c = columns;
 		board = new int[rows][columns];
 		test = new Al[rows][columns];
+		time = new JLabel("0");
+		bombCount = mines;
+		bomb = new JLabel(Integer.toString(bombCount));
+		count = 0;
+		mainpanel = new JPanel();
+		mainpanel.setLayout(new BoxLayout(mainpanel, BoxLayout.Y_AXIS));
+
+		
+		JFrame frame = new JFrame("Minesweeper");
 		
 		generateMines(mines);
 		countMinesNearSquare();
 		
-		
-		JFrame frame = new  JFrame("Easy Mode");
-		mainpanel= new JPanel();
-		mainpanel.setLayout(new BoxLayout(mainpanel,BoxLayout.Y_AXIS));
-		
-		JPanel sub1 = new JPanel();
-		
 		//Top Bomb counter and Timer
-		count =0; 
-	    bombCount=mines;
+		JPanel timeDisplay = new JPanel();
 		JLabel bombs = new JLabel("Bombs left:");
-		bomb=new JLabel(Integer.toString(bombCount));
-		sub1.add(bombs);
-		sub1.add(bomb);
-		sub1.add(new JLabel(" ===== "));
 		JLabel t = new JLabel("Time:");
-		time= new JLabel("0");		
-		sub1.add(t);
-		sub1.add(time);	
-		
+		timeDisplay.add(bombs);
+		timeDisplay.add(bomb);
+		timeDisplay.add(new JLabel(" ===== "));
+		timeDisplay.add(t);
+		timeDisplay.add(time);
+
 		timer = new Timer(1000, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				count++;
-		        if (count < 100000) {
-		            time.setText(Integer.toString(count));
-		          } else {
-		            ((Timer) (e.getSource())).stop();
-		          }
+				if (count < 100000) {
+					time.setText(Integer.toString(count));
+				} else {
+					((Timer) (e.getSource())).stop();
+				}
 			}
 		});
-	    timer.setInitialDelay(1000);
-	    timer.start();
 		
-		mainpanel.add(sub1);
-		
-		
-		JPanel sub2 = new JPanel();
-		//adding all the buttons for minesweeper
+		timer.setInitialDelay(1000);
+		timer.start();
+
+		JPanel cellDisplay = new JPanel();
 		GridLayout layout = new GridLayout(rows, columns);
-		sub2.setLayout(layout);
-		
+		cellDisplay.setLayout(layout);
+
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				JButton button = new JButton("");
 				switch (board[i][j]) {
-					case 0:{
-						Al a= new Al(button, "0",i,j);
-						button.addActionListener(a);
-						button.addMouseListener(a);
-						break;
-					}
-					case 1:{
-/*						ImageIcon image = null;
-						try {
-							image = new ImageIcon(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Minesweeper_1.svg/480px-Minesweeper_1.svg.png"));
-						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						case 0: {
+							Al a = new Al(button, "0", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
 						}
-						Al a = new Al(button, image);*/
-						Al a= new Al(button, "1",i,j);
-						button.addActionListener(a);
-						button.addMouseListener(a);
-						break;
-					}
-					case 2:{
-	/*					ImageIcon image = null;
-						try {
-							image = new ImageIcon(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Minesweeper_2.svg/480px-Minesweeper_2.svg.png"));
-						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						case 1: {
+							/*
+							 * ImageIcon image = null; try { image = new ImageIcon(new URL(
+							 * "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Minesweeper_1.svg/480px-Minesweeper_1.svg.png"
+							 * )); } catch (MalformedURLException e) { // TODO Auto-generated catch block
+							 * e.printStackTrace(); } Al a = new Al(button, image);
+							 */
+							Al a = new Al(button, "1", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
 						}
-						Al a = new Al(button, image);*/
-						Al a= new Al(button, "2",i,j);
-						button.addActionListener(a);
-						button.addMouseListener(a);
-						break;
-					}
-					case 3:{
-				/*		ImageIcon image = null;
-						try {
-							image = new ImageIcon(new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Minesweeper_3.svg/480px-Minesweeper_3.svg.png"));
-						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						case 2: {
+							Al a = new Al(button, "2", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
 						}
-						Al a = new Al(button, image);*/
-						Al a= new Al(button, "3",i,j);
-						button.addActionListener(a);
-						button.addMouseListener(a);
-						break;
-					}
-					case 10:{
-			/*			ImageIcon image = null;
-						try {
-							image = new ImageIcon(new URL("https://giantbomb1.cbsistatic.com/uploads/scale_small/8/87790/3216800-icon_mine.png"));
-						} catch (MalformedURLException e) {
-							// TODO Auto-generated catch block
-							System.out.println("image not found");
+						case 3: {
+							Al a = new Al(button, "3", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
 						}
-						
-						Al a = new Al(button, image);*/
-						Al a= new Al(button, "bomb",i,j);
-						button.addActionListener(a);
-						button.addMouseListener(a);
-						break;
-						
-					}
-					
+						case 4: {
+							Al a = new Al(button, "4", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+						case 5: {
+							Al a = new Al(button, "5", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+						case 6: {
+							Al a = new Al(button, "6", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+						case 7: {
+							Al a = new Al(button, "7", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+						case 8: {
+							Al a = new Al(button, "8", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+						case 10: {
+							Al a = new Al(button, "bomb", i, j);
+							button.addActionListener(a);
+							button.addMouseListener(a);
+							break;
+						}
+
 				}
-/*				JButton button = new JButton("");
-				ImageIcon image = new ImageIcon("C:\\Users\\kevin\\Documents\\480px-Minesweeper_1.svg.png");
-				Al a = new Al(button,image);
-				button.addActionListener(a);	*/			
-			sub2.add(button);
-			Al temp = (Al) button.getActionListeners()[0];
-			test[i][j]=temp;
+				/*
+				 * JButton button = new JButton(""); ImageIcon image = new
+				 * ImageIcon("C:\\Users\\kevin\\Documents\\480px-Minesweeper_1.svg.png"); Al a =
+				 * new Al(button,image); button.addActionListener(a);
+				 */
+				cellDisplay.add(button);
+				Al temp = (Al) button.getActionListeners()[0];
+				test[i][j] = temp;
 			}
 		}
-		sub2.setPreferredSize(new Dimension(800,1000));
-		mainpanel.add(sub2);
+		cellDisplay.setPreferredSize(new Dimension(800, 1000));
 		
-		
+		//Display Everything
+		mainpanel.add(timeDisplay);
+		mainpanel.add(cellDisplay);
 		frame.add(mainpanel);
 		frame.setSize(800, 800);
 		frame.setVisible(true);
-	
+
 	}
-	
-	
-	//squares that don't contain mines need a number to indicate how many mines are diagonally/directly adjacent to it
+
+	// squares that don't contain mines need a number to indicate how many mines are
+	// diagonally/directly adjacent to it
 	void countMinesNearSquare() {
 		for (int i = 0; i < r; i++) {
 			for (int j = 0; j < c; j++) {
 				if (board[i][j] == 10) {
 					continue;
-				}
-				else {
+				} else {
 					if (i != 0 && board[i - 1][j] == 10) {
 						board[i][j]++;
 					}
@@ -205,170 +202,173 @@ public class GamePlay {
 			}
 		}
 	}
-	
-	//spawn an exact amount of mines as suggested in class constructor
+
+	// spawn an exact amount of mines as suggested in class constructor
 	void generateMines(int m) {
 		for (int i = m; i > 0; i--) {
-			int rowr = (int)(Math.random() * r);
-			int columnr = (int)(Math.random() * c);
+			int rowr = (int) (Math.random() * r);
+			int columnr = (int) (Math.random() * c);
 			if (board[rowr][columnr] == 10) {
 				i++;
 				continue;
-			}
-			else {
+			} else {
 				board[rowr][columnr] = 10;
 			}
 		}
 	}
-	//test the grid
+
+	// test the grid
 	void print() {
-		for (int[] i: board) {
-			for (int in: i) {
+		for (int[] i : board) {
+			for (int in : i) {
 				System.out.printf("%4d", in);
 			}
 			System.out.println();
 		}
 	}
-	
-	public static void main(String[] args) {
-		GamePlay g = new GamePlay(10, 9, 9);
-		g.print();
-	}
-	
-	//ActionListener to change icon and disable further presses
-class Al implements ActionListener, MouseListener{
-	JButton button;
-	ImageIcon i;
-	String s ="";
-	int r ;
-	int c;
-	boolean flag= false;
-	public Al(JButton b, ImageIcon i) {
-		button = b;
-		this.i=i;
-	}
-	public Al(JButton b, String s) {
-		button = b; 
-		this.s=s;
-		i=null;
-	}
-	public Al(JButton b, String s,int r, int c) {
-		button = b;
-		this.s=s;
-		this.r=r;
-		this.c=c;
-	}
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-			//need a pic of numbers
-			if(i==null) {
-				button.setText(s);
-				if(s.equals("bomb")) {
-					//stop the game
+
+
+	// ActionListener to change icon and disable further presses
+	class Al implements ActionListener, MouseListener {
+		
+		JButton button;
+		ImageIcon i;
+		String s = "";
+		int r, c;
+		boolean flag = false;
+
+		public Al(JButton b, ImageIcon i) {
+			button = b;
+			this.i = i;
+		}
+
+		public Al(JButton b, String s) {
+			button = b;
+			this.s = s;
+			i = null;
+		}
+
+		public Al(JButton b, String s, int r, int c) {
+			button = b;
+			this.s = s;
+			this.r = r;
+			this.c = c;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// need a pic of numbers
+			if (i == null) {
+				if (!s.equals("0")) {
+					button.setText(s);
 				}
-				if(s.equals("0")) {
-					testThis(r,c);
+				if (s.equals("bomb")) {
+					// stop the game
 				}
-			}
-			else {
+				if (s.equals("0")) {
+					testThis(r, c);
+				}
+			} else {
 				button.setIcon(i);
 			}
-
-			button.removeActionListener(this);
-			button.removeMouseListener(this);
-	}
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		if(arg0.getButton()==MouseEvent.BUTTON3) {
-			
-		if(flag) {
-			button.setText("");
-			bomb.setText(Integer.toString(++bombCount));
-			button.setEnabled(true);
-			flag=false;
-		}
-		else {
-			button.setText("flagged");
-			bomb.setText(Integer.toString(--bombCount));
 			button.setEnabled(false);
-			flag=true;
-		}}
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
 		}
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub		
-	}
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-	}
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub	
-	}
-	public void testThis(int r, int c) {
-		
-		if(r>=0&&c>=0&&r<test.length&&c<test.length) {
-			if(board[r][c]==0) {
-				button.setText(s);
-				button.setEnabled(false);
-				board[r][c]=-1;
-			}
-			else if(board[r][c]!=-1){
-				button.setText(s);
-				return;
-			}
-			else{
-				return;
-			}
-			
-			if(r<test.length-1) {
-				test[r+1][c].testThis(r+1, c);
-				if(c>0) {
-					test[r+1][c-1].testThis(r+1, c-1);
-				}
-				if(c<test.length-1) {
-					test[r+1][c+1].testThis(r+1,c+1);
-				}
-				}
-			
-			if(r>0) {
-				test[r-1][c].testThis(r-1,c);
-				if(c>0) {
-					test[r-1][c-1].testThis(r+1, c-1);
-				}
-				if(c<test.length-1) {
-					test[r-1][c+1].testThis(r+1,c+1);
-				}}
-			if(c>0) {
-				test[r][c-1].testThis(r, c-1);
-				if(r>0) {
-					test[r-1][c-1].testThis(r-1,c-1);
-				}
-				if(r<test.length-1) {
-					test[r+1][c-1].testThis(r+1, c-1);
-				}
-			}
-			if(c<test.length-1) {
-				test[r][c+1].testThis(r, c+1);
-				if(r>0) {
-					test[r-1][c+1].testThis(r-1,c-1);
-				}
-				if(r<test.length-1) {
-					test[r+1][c+1].testThis(r+1, c-1);
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			if (arg0.getButton() == MouseEvent.BUTTON3) {
+
+				if (flag) {
+					button.setText("");
+					bomb.setText(Integer.toString(++bombCount));
+					button.setEnabled(true);
+					flag = false;
+				} else {
+					button.setText("flagged");
+					bomb.setText(Integer.toString(--bombCount));
+					button.setEnabled(false);
+					flag = true;
 				}
 			}
 
-	
 		}
-		
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+
+		public void testThis(int r, int c) {
+
+			if (r >= 0 && c >= 0 && r < test.length && c < test.length) {
+				
+				if (board[r][c] == 0) {
+					button.setEnabled(false);
+					board[r][c] = -1;
+				} else if (board[r][c] != -1) {
+					button.setText(s);
+					button.setEnabled(false);
+					return;
+				} else {
+					return;
+				}
+
+				if (r < test.length - 1) {
+					test[r + 1][c].testThis(r + 1, c);
+					if (c > 0) {
+						test[r + 1][c - 1].testThis(r + 1, c - 1);
+					}
+					if (c < test.length - 1) {
+						test[r + 1][c + 1].testThis(r + 1, c + 1);
+					}
+				}
+
+				if (r > 0) {
+					test[r - 1][c].testThis(r - 1, c);
+					if (c > 0) {
+						test[r - 1][c - 1].testThis(r + 1, c - 1);
+					}
+					if (c < test.length - 1) {
+						test[r - 1][c + 1].testThis(r + 1, c + 1);
+					}
+				}
+				if (c > 0) {
+					test[r][c - 1].testThis(r, c - 1);
+					if (r > 0) {
+						test[r - 1][c - 1].testThis(r - 1, c - 1);
+					}
+					if (r < test.length - 1) {
+						test[r + 1][c - 1].testThis(r + 1, c - 1);
+					}
+				}
+				if (c < test.length - 1) {
+					test[r][c + 1].testThis(r, c + 1);
+					if (r > 0) {
+						test[r - 1][c + 1].testThis(r - 1, c - 1);
+					}
+					if (r < test.length - 1) {
+						test[r + 1][c + 1].testThis(r + 1, c - 1);
+					}
+				}
+
+			}
+
+		}
 	}
 }
-}
-
